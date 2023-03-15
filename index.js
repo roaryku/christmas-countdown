977bc7add50db86ae90eefb173d00de2e549ed00
Rodika <raryku@yahoo.com>
2023-03-14 22:19:56 -0700

    js fixed

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function christmasCountdown(){
-    const christmasDate = new Date ("december 25, 2022 00:00");
+    const christmasDate = new Date ("december 25, 2023 00:00");
     const now = new Date();
     const diff = christmasDate - now;
     
