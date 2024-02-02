e3f1ea39f3a0f55496e625b3c847d61ac7c993e1
Rodika Aryku <119984393+roaryku@users.noreply.github.com>
2024-02-02 12:24:29 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function christmasCountdown(){
-    const christmasDate = new Date ("december 25, 2023 00:00");
+    const christmasDate = new Date ("december 25, 2024 00:00");
     const now = new Date();
     const diff = christmasDate - now;
     
